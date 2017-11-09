8c01a1aedcdf83b7c3f7bc2ad73a92d15ca223d5
Michael Myung <michael.h.myung@gmail.com>
2017-11-09 21:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 let currentUser = 'Myungbeans'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
