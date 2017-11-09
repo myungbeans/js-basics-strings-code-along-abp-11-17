a0c22fb69f74f22708d86dd184d5606771d55f8d
Michael Myung <michael.h.myung@gmail.com>
2017-11-09 21:03:30 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,5 +0,0 @@
-// Write your code in this file!
-let currentUser = 'Myungbeans'
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-let shortGreeting = `Welcome ${currentUser}!`
